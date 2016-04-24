55e86ba4ef3f653909f11fc75770828f9018b4f8
keyswor <keyswor1@gmail.com>
2016-04-24 14:15:55 +0545

    deleting a list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,33 +1,28 @@
 class ListsController < ApplicationController
-  def index
-    @lists = List.all
-  end
-
-  def new
-
-    @list = List.new
-  end
-  def create
-    @list = List.create(lists_param)
-    if @list.save
-      redirect_to lists_path
-    else
-      render("new")
-    end
-  end
-
-  def edit
-    @list = List.find(params[:id])
-    
-
-
-  end
-
-
-
-  private
-  def lists_param
-    params.require(:list).permit(:title)
-  end
-
+def index
+@lists = List.all
+end
+def new
+@list = List.new
+end
+def create
+@list = List.create(lists_param)
+if @list.save
+redirect_to lists_path
+else
+render("new")
+end
+end
+def edit
+@list = List.find(params[:id])
+end
+def destroy
+@list = List.find(params[:id])
+@list.destroy
+redirect_to lists_path
+end
+private
+def lists_param
+params.require(:list).permit(:title)
+end
 end
