5c0eddd2e208ea63d77cc10dbc4d66ca3e5f2d6e
keyswor <keyswor1@gmail.com>
2016-04-24 14:09:55 +0545

    adding a list

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,33 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+
+    @list = List.new
+  end
+  def create
+    @list = List.create(lists_param)
+    if @list.save
+      redirect_to lists_path
+    else
+      render("new")
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+    
+
+
+  end
+
+
+
+  private
+  def lists_param
+    params.require(:list).permit(:title)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   root 'home#index'
+  resources :lists
+  # get 'lists/index'
+
+  # get 'lists/new'
+
+  # get 'lists/edit'
+
+  # get 'lists/create'
+
+  # get 'lists/update'
+
+  # get 'lists/destroy'
+
+  # root 'home#index'
 
   
 end
